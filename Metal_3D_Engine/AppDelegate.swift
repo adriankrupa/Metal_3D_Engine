a340625e388125234adcaa261ddd633b40eecc1f
Adrian Krupa <adrian.krupa91@gmail.com>
2015-12-06 15:00:04 +0100

    Initial Commit

---
+++ Metal_3D_Engine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Metal_3D_Engine
+//
+//  Created by Adrian Krupa on 06.12.2015.
+//  Copyright © 2015 Adrian Krupa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Metal_3D_Engine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Metal_3D_Engine
+//
+//  Created by Adrian Krupa on 06.12.2015.
+//  Copyright © 2015 Adrian Krupa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

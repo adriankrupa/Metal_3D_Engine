c6a687a43da6e98e2c5fc4e74de9eba039156a3a
Adrian Krupa <adrian.krupa91@gmail.com>
2015-12-06 16:08:47 +0100

    clearing screen

--- Metal_3D_Engine/ViewController.swift
+++ Metal_3D_Engine/ViewController.swift
@@ -6,22 +6,57 @@
 //  Copyright © 2015 Adrian Krupa. All rights reserved.
 //
 
+#if os(OSX)
 import Cocoa
+typealias BaseClass = NSViewController
+#else
+import UIKit
+typealias BaseClass = UIViewController
+#endif
 
-class ViewController: NSViewController {
+import MetalKit
+
+class ViewController: BaseClass, MTKViewDelegate {
+    
+    let device: MTLDevice = MTLCreateSystemDefaultDevice()!
+    
+    var commandQueue: MTLCommandQueue! = nil
+    var pipelineState: MTLRenderPipelineState! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let view = self.view as! MTKView
+        view.delegate = self
+        view.device = device
+        view.sampleCount = 4
+        
+        commandQueue = device.newCommandQueue()
+        commandQueue.label = "main command queue"
     }
+    
+    func drawInMTKView(view: MTKView) {
+        let commandBuffer = commandQueue.commandBuffer()
+        commandBuffer.label = "Frame command buffer"
+        
+        if let renderPassDescriptor = view.currentRenderPassDescriptor, currentDrawable = view.currentDrawable
+        {
+            renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(1.0, 1.0, 0.0, 1.0);
+            let renderEncoder = commandBuffer.renderCommandEncoderWithDescriptor(renderPassDescriptor)
+            renderEncoder.label = "render encoder"
+            
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
+            renderEncoder.endEncoding()
+            
+            commandBuffer.presentDrawable(currentDrawable)
         }
+
+        commandBuffer.commit()
+
     }
 
-
+    func mtkView(view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
 }
 
